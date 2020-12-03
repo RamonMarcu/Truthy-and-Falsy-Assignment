81cac733450081892abf29c4ff0eba1e78f073ce
Ramon Marcu <70721540+RamonMarcu@users.noreply.github.com>
2020-12-03 20:18:54 +0200

    Add files via upload

---
+++ Truthy and Falsy Assignment.js
@@ -0,0 +1,22 @@
+let tool = '';
+
+// Using short circuit evaluation to assign  writingUtensil variable below:
+
+let writingUtensil = tool || 'pen';
+
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+/* if run, the console displays: The pen is mightier than the sword. */
+
+
+let tool = 'marker';
+
+// Using short circuit evaluation to assign  writingUtensil variable below:
+
+let writingUtensil = tool || 'pen';
+
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+/* if run, the console displays: The marker is mightier than the sword. */
